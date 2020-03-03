4a586c19dea47f06e1e3f1a18bed86cda65f7792
jr.fernando@hotmail.com <m218337@CORP.BRADESCO.COM.BR>
2020-03-03 11:23:13 -0300

    Implementação de testes e camada service

---
+++ wallet/src/main/java/com/wallet/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.wallet.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wallet.entity.User;
+import com.wallet.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public User save(User u) {
+		return userRepository.save(u);
+	}
+
+	@Override
+	public Optional<User> findByEmailEquals(String email) {
+		return userRepository.findByEmailEquals(email);
+	}
+
+}
